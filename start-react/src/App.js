86a1a656b076680c31a7a37c528b2e7a78ef213f
Dmitriy <korsun71dima@mail.ru>
2024-10-07 21:26:21 +0300

    Without JSX

--- start-react/src/App.js
+++ start-react/src/App.js
@@ -1,26 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 // Декларативный стиль
 export const App = () => {
 	const year = new Date().getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{year}
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			year,
+		),
 	);
 };
