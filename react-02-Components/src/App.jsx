972511ddf75143e1ae140936328d224e162ccf38
Dmitriy <korsun71dima@mail.ru>
2025-01-09 18:51:38 +0300

    finish

--- react-02-Components/src/App.jsx
+++ react-02-Components/src/App.jsx
@@ -1,54 +1,68 @@
-import { useState } from 'react'
-import styles from './app.module.css'
+import { useState } from 'react';
+import styles from './app.module.css';
 
 function App() {
-  const [value, setValue] = useState("")
-  const [list, setList] = useState([])
-  const [error, setError] = useState("")
+	const [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	const [error, setError] = useState('');
 
-  const onInputButtonClick = () => {
-	let promptValue = prompt('Введите значение');
-	console.log(promptValue);
-	promptValue.length < 3 ?
-	setError('Введенное значение должно содержать минимум 3 символа') :
-	setValue(promptValue) & setError('');
-  }
+	const onInputButtonClick = () => {
+		let promptValue = prompt('Введите значение');
+		console.log(promptValue);
+		promptValue.length < 3
+			? setError('Введенное значение должно содержать минимум 3 символа')
+			: setValue(promptValue) & setError('');
+	};
 
-  const isValueVaild = Boolean (value.length >= 3);
+	const isValueVaild = Boolean(value.length >= 3);
 
-  const onAddButtonClick = () => {
-	let id = Date.now();
-	let date = new Date().toLocaleString();
-	console.log(date)
-	const updateList = [...list, {id, value, date}]
+	const onAddButtonClick = () => {
+		let id = Date.now();
+		let date = new Date().toLocaleString();
+		console.log(date);
+		const updateList = [...list, { id, value, date }];
 
 		if (value.length >= 3) {
-		setList(updateList) & setError('') & setValue('')
+			setList(updateList) & setError('') & setValue('');
+		}
+		console.log(updateList);
 	};
-	console.log(updateList)
-  }
 
-  return (
-	<div className={styles.app}>
-    <h1 className={styles['page-heading']}>Ввод значения</h1>
-    <p className={styles["no-margin-text"]}>
-      Текущее значение <code>value</code>: "<output className={styles["current-value"]}>{value}</output>"
-    </p>
-    <div className={styles.error}>{error}</div>
-    <div className={styles["buttons-container"]}>
-      <button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
-      <button className={styles.button} disabled={!isValueVaild} onClick={onAddButtonClick}>Добавить в список</button>
-    </div>
-    <div className={styles["list-container"]}>
-      <h2 className={styles["list-heading"]}>Список:</h2>
-      {!list.length && <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>}
-      <ul className={styles.list}>
-		{list.map(({ id, value, date }) =>
-        <li className={styles["list-item"]} key={id}>{value} {date}</li>)}
-      </ul>
-    </div>
-  </div>
-  )
+	return (
+		<div className={styles.app}>
+			<h1 className={styles['page-heading']}>Ввод значения</h1>
+			<p className={styles['no-margin-text']}>
+				Текущее значение <code>value</code>: "
+				<output className={styles['current-value']}>{value}</output>"
+			</p>
+			<div className={styles.error}>{error}</div>
+			<div className={styles['buttons-container']}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button
+					className={styles.button}
+					disabled={!isValueVaild}
+					onClick={onAddButtonClick}
+				>
+					Добавить в список
+				</button>
+			</div>
+			<div className={styles['list-container']}>
+				<h2 className={styles['list-heading']}>Список:</h2>
+				{!list.length && (
+					<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+				)}
+				<ul className={styles.list}>
+					{list.map(({ id, value, date }) => (
+						<li className={styles['list-item']} key={id}>
+							{value} {date}
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
